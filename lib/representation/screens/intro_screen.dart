21f23df76450199ab39186d27b6f8fee419dab1a
nguyenkhanh96 <nguyenkhanh96.bk@gmail.com>
2022-12-31 10:43:18 +0700

    Make Onboarding Screen - Part 2

--- lib/representation/screens/intro_screen.dart
+++ lib/representation/screens/intro_screen.dart
@@ -1,7 +1,12 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_app_2022_travel_app/core/constants/dimension_constants.dart';
 import 'package:flutter_app_2022_travel_app/core/helpers/asset.helper.dart';
 import 'package:flutter_app_2022_travel_app/core/helpers/image_helper.dart';
+import 'package:flutter_app_2022_travel_app/representation/screens/main_app.dart';
+import 'package:flutter_app_2022_travel_app/representation/widgets/button_widget.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 import '../../core/constants/textstyle_constants.dart';
 
@@ -17,15 +22,29 @@ class IntroScreen extends StatefulWidget {
 class _IntroScreenState extends State<IntroScreen> {
   final PageController _pageController = PageController();
 
+  final StreamController<int> _pageStreamController =
+      StreamController<int>.broadcast();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _pageController.addListener(() {
+      _pageStreamController.add(_pageController.page!.toInt());
+    });
+  }
+
   Widget _buildItemIntroScreen(String image, String title, String description,
       AlignmentGeometry alignment) {
     return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Container(
           alignment: alignment,
           child: ImageHelper.loadFromAsset(
             image,
-            height: 375,
+            height: 400,
             fit: BoxFit.fitHeight,
           ),
         ),
@@ -37,6 +56,7 @@ class _IntroScreenState extends State<IntroScreen> {
             horizontal: kMediumPadding,
           ),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Text(
                 title,
@@ -59,26 +79,72 @@ class _IntroScreenState extends State<IntroScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView(
-        controller: _pageController,
+      body: Stack(
         children: [
-          _buildItemIntroScreen(
-            AssetHelper.intro1,
-            "Book a flight",
-            "Found a flight that matches your destination and schedule? Book it instantly.",
-            Alignment.centerRight,
+          PageView(
+            controller: _pageController,
+            children: [
+              _buildItemIntroScreen(
+                AssetHelper.intro1,
+                "Book a flight",
+                "Found a flight that matches your destination and schedule? Book it instantly.",
+                Alignment.centerRight,
+              ),
+              _buildItemIntroScreen(
+                AssetHelper.intro2,
+                "Find a hotel room",
+                "Select the day, book your room. We give you the best price.",
+                Alignment.center,
+              ),
+              _buildItemIntroScreen(
+                AssetHelper.intro3,
+                "Enjoy your trip",
+                "Easy discovering new places and share these between your friends and travel together.",
+                Alignment.centerLeft,
+              ),
+            ],
           ),
-          _buildItemIntroScreen(
-            AssetHelper.intro2,
-            "Find a hotel room",
-            "Select the day, book your room. We give you the best price.",
-            Alignment.center,
-          ),
-          _buildItemIntroScreen(
-            AssetHelper.intro3,
-            "Enjoy your trip",
-            "Easy discovering new places and share these between your friends and travel together.",
-            Alignment.centerLeft,
+          Positioned(
+            left: kMediumPadding,
+            right: kMediumPadding,
+            bottom: kMediumPadding * 3,
+            child: Row(
+              children: [
+                Expanded(
+                  flex: 6,
+                  child: SmoothPageIndicator(
+                    controller: _pageController,
+                    count: 3,
+                    effect: const ExpandingDotsEffect(
+                      dotWidth: kMinPadding,
+                      dotHeight: kMinPadding,
+                      activeDotColor: Colors.orange,
+                    ),
+                  ),
+                ),
+                StreamBuilder<int>(
+                    initialData: 0,
+                    stream: _pageStreamController.stream,
+                    builder: (context, snapshot) {
+                      return Expanded(
+                        flex: 4,
+                        child: ButtonWidget(
+                          title: snapshot.data != 2 ? "Next" : "Get started",
+                          ontap: () {
+                            if (_pageController.page != 2) {
+                              _pageController.nextPage(
+                                  duration: const Duration(milliseconds: 200),
+                                  curve: Curves.easeIn);
+                            } else {
+                              Navigator.of(context)
+                                  .pushNamed(MainApp.routeName);
+                            }
+                          },
+                        ),
+                      );
+                    }),
+              ],
+            ),
           ),
         ],
       ),
