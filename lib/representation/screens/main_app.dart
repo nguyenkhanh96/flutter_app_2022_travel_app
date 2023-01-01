ff92ae48a7e98d4b1905e64cc427d8df62ccfb96
nguyenkhanh96 <nguyenkhanh96.bk@gmail.com>
2023-01-01 23:35:28 +0700

    #8 - Create Fancy BottomNavigationBar

--- lib/representation/screens/main_app.dart
+++ lib/representation/screens/main_app.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_app_2022_travel_app/core/constants/color_constants.dart';
+import 'package:flutter_app_2022_travel_app/core/constants/dimension_constants.dart';
+import 'package:flutter_app_2022_travel_app/representation/screens/home_screen.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:salomon_bottom_bar/salomon_bottom_bar.dart';
 
 class MainApp extends StatefulWidget {
   const MainApp({super.key});
@@ -12,13 +17,64 @@ class MainApp extends StatefulWidget {
 }
 
 class _MainAppState extends State<MainApp> {
+  int _currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          "Main App",
-        ),
+      backgroundColor: Colors.white,
+      body: IndexedStack(
+        index: _currentIndex,
+        children: [
+          HomeScreen(),
+          Container(
+            color: Colors.blue,
+          ),
+          Container(
+            color: Colors.brown,
+          ),
+          Container(
+            color: Colors.yellow,
+          ),
+        ],
+      ),
+      bottomNavigationBar: SalomonBottomBar(
+        currentIndex: _currentIndex,
+        onTap: (index) => setState(() => _currentIndex = index),
+        selectedItemColor: ColorPalette.primaryColor,
+        unselectedItemColor: ColorPalette.primaryColor.withOpacity(0.2),
+        margin: EdgeInsets.symmetric(
+            horizontal: kMediumPadding, vertical: kDefaultPadding),
+        items: [
+          SalomonBottomBarItem(
+            icon: Icon(
+              FontAwesomeIcons.house,
+              size: kDefaultIconSize,
+            ),
+            title: Text("Home"),
+          ),
+          SalomonBottomBarItem(
+            icon: Icon(
+              FontAwesomeIcons.solidHeart,
+              size: kDefaultIconSize,
+            ),
+            title: Text("Likes"),
+          ),
+          SalomonBottomBarItem(
+            icon: Icon(
+              FontAwesomeIcons.briefcase,
+              size: kDefaultIconSize,
+            ),
+            title: Text("Booking"),
+          ),
+          SalomonBottomBarItem(
+            icon: Icon(
+              FontAwesomeIcons.solidUser,
+              size: kDefaultIconSize,
+            ),
+            title: Text("Profile"),
+          ),
+        ],
       ),
     );
   }
