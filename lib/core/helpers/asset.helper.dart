bc97c9306c06730b02465e6530a961e005eb90bf
nguyenkhanh96 <nguyenkhanh96.bk@gmail.com>
2023-01-02 22:32:20 +0700

    #12 - Make HomeScreen App

--- lib/core/helpers/asset.helper.dart
+++ lib/core/helpers/asset.helper.dart
@@ -8,4 +8,7 @@ class AssetHelper {
   static const String oval_1 = 'assets/images/oval_1.png';
   static const String oval_2 = 'assets/images/oval_2.png';
   static const String persion = 'assets/images/persion.png';
+  static const String icoHotel = 'assets/icons/ico_hotel.png';
+  static const String icoPlane = 'assets/icons/ico_plane.png';
+  static const String icoHotelPlane = 'assets/icons/ico_hotel_plane.png';
 }
--- lib/representation/screens/home_screen.dart
+++ lib/representation/screens/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter_app_2022_travel_app/core/constants/color_constants.dart'
 import 'package:flutter_app_2022_travel_app/core/constants/dimension_constants.dart';
 import 'package:flutter_app_2022_travel_app/core/helpers/asset.helper.dart';
 import 'package:flutter_app_2022_travel_app/core/helpers/image_helper.dart';
+import 'package:flutter_app_2022_travel_app/representation/screens/hotel_screen.dart';
 import 'package:flutter_app_2022_travel_app/representation/widgets/app_bar_container.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -14,6 +15,32 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  Widget _builtItemCategory(
+      Widget icon, Color color, Function() onTap, String title) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            padding: EdgeInsets.symmetric(
+              vertical: kMediumPadding,
+            ),
+            decoration: BoxDecoration(
+              color: color.withOpacity(0.2),
+              borderRadius: BorderRadius.circular(kItemPadding),
+            ),
+            child: icon,
+          ),
+          SizedBox(
+            height: kItemPadding,
+          ),
+          Text(title)
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return AppBarContainerWidget(
@@ -70,7 +97,85 @@ class _HomeScreenState extends State<HomeScreen> {
           ],
         ),
       ),
-      child: Container(),
+      child: Column(
+        children: [
+          TextField(
+            decoration: InputDecoration(
+              hintText: 'Search your destination',
+              prefixIcon: Padding(
+                padding: EdgeInsets.all(kTopPadding),
+                child: Icon(
+                  FontAwesomeIcons.magnifyingGlass,
+                  color: Colors.black,
+                  size: kDefaultPadding,
+                ),
+              ),
+              filled: true,
+              fillColor: Colors.white,
+              border: OutlineInputBorder(
+                borderSide: BorderSide.none,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(
+                    kItemPadding,
+                  ),
+                ),
+              ),
+              contentPadding: EdgeInsets.symmetric(
+                horizontal: kItemPadding,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: kDefaultPadding,
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: _builtItemCategory(
+                    ImageHelper.loadFromAsset(
+                      AssetHelper.icoHotel,
+                      width: kBottomBarIconSize,
+                      height: kBottomBarIconSize,
+                    ),
+                    Color(0xffFE9C5E),
+                    () => {
+                          Navigator.of(context)
+                              .pushNamed(HotelScreen.routeName),
+                        },
+                    "Hotels"),
+              ),
+              SizedBox(
+                width: kDefaultPadding,
+              ),
+              Expanded(
+                child: _builtItemCategory(
+                    ImageHelper.loadFromAsset(
+                      AssetHelper.icoPlane,
+                      width: kBottomBarIconSize,
+                      height: kBottomBarIconSize,
+                    ),
+                    Color(0xffF77777),
+                    () => {},
+                    "Flights"),
+              ),
+              SizedBox(
+                width: kDefaultPadding,
+              ),
+              Expanded(
+                child: _builtItemCategory(
+                    ImageHelper.loadFromAsset(
+                      AssetHelper.icoHotelPlane,
+                      width: kBottomBarIconSize,
+                      height: kBottomBarIconSize,
+                    ),
+                    Color(0xff3EC8BC),
+                    () => {},
+                    "All"),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
