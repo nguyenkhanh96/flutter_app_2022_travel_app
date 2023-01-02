9f58e39d55b8f875df08bf0cb8f8c69217e82025
nguyenkhanh96 <nguyenkhanh96.bk@gmail.com>
2023-01-02 14:18:48 +0700

    #10 - Create Custom AppBar - Part 2

--- lib/representation/screens/home_screen.dart
+++ lib/representation/screens/home_screen.dart
@@ -12,6 +12,9 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return AppBarContainerWidget(
+      titleString: "Home",
+      implementLeading: true,
+      implementTraling: true,
       child: Container(
         color: Colors.amber,
       ),
--- lib/representation/widgets/app_bar_container.dart
+++ lib/representation/widgets/app_bar_container.dart
@@ -5,11 +5,23 @@ import 'package:flutter_app_2022_travel_app/core/constants/color_constants.dart'
 import 'package:flutter_app_2022_travel_app/core/constants/dimension_constants.dart';
 import 'package:flutter_app_2022_travel_app/core/helpers/asset.helper.dart';
 import 'package:flutter_app_2022_travel_app/core/helpers/image_helper.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class AppBarContainerWidget extends StatelessWidget {
-  const AppBarContainerWidget({super.key, required this.child});
+  const AppBarContainerWidget({
+    super.key,
+    required this.child,
+    this.title,
+    this.implementLeading = false,
+    this.titleString,
+    this.implementTraling = false,
+  });
 
   final Widget child;
+  final Widget? title;
+  final String? titleString;
+  final bool implementLeading;
+  final bool implementTraling;
 
   @override
   Widget build(BuildContext context) {
@@ -24,6 +36,56 @@ class AppBarContainerWidget extends StatelessWidget {
               elevation: 0,
               toolbarHeight: 90,
               backgroundColor: ColorPalette.backgroundScaffoldColor,
+              title: title ??
+                  Row(
+                    children: [
+                      if (implementLeading)
+                        Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(kDefaultPadding),
+                            ),
+                            color: Colors.white,
+                          ),
+                          padding: EdgeInsets.all(kItemPadding),
+                          child: Icon(
+                            FontAwesomeIcons.arrowLeft,
+                            color: Colors.black,
+                            size: kDefaultIconSize,
+                          ),
+                        ),
+                      Expanded(
+                        child: Center(
+                          child: Column(
+                            children: [
+                              Text(
+                                titleString ?? '',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      if (implementTraling)
+                        Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(kDefaultPadding),
+                            ),
+                            color: Colors.white,
+                          ),
+                          padding: EdgeInsets.all(kItemPadding),
+                          child: Icon(
+                            FontAwesomeIcons.bars,
+                            color: Colors.black,
+                            size: kDefaultIconSize,
+                          ),
+                        ),
+                    ],
+                  ),
               flexibleSpace: Stack(
                 children: [
                   Container(
